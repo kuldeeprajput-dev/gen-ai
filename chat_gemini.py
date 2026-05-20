2e47f671f34e5861320a95d034746c46f9f35d17
Kuldeep <kunalrajput67718@gmail.com>
2026-05-20 14:49:10 +0530

    chat with gemini

---
+++ chat_gemini.py
@@ -0,0 +1,13 @@
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+response =  client.models.generate_content(
+    model='gemini-2.5-flash', contents='Tell me a story in 300 words.'
+)
+
+print(response.text)
